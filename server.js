ae953249c55ec0a47b09e2db38daaf5acf8ad049
alessiotinaglia <alessio.tina1998@gmail.com>
2024-12-09 12:04:35 +0100

    fine punto due

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// Middleware
+// global Middleware
 
 // per gestire il body JSON
 app.use(express.json());
@@ -18,13 +18,21 @@ const postsRouter = require("./router/posts.js");
 // Importa da middlewares
 const checkTime = require("./middlewares/checkTime.js");
 
+// Importa da middlewares
+const errorsHandler = require("./middlewares/errorsHandler.js");
+
 // Rotte api
 app.use("/posts", postsRouter);
 
 //rotta fallback
-app.all("*", (req, res) => {
-  res.status(404).json({ error: "404", message: "Not Found !" });
-});
+// app.all("*", (req, res) => {
+//   res.status(404).json({ error: "404", message: "Not Found !" });
+// });
+
+
+
+// gestione degli errori (deve essere sempre la penultima)
+app.use(errorsHandler);
 
 // In ascolto sulla porta 3000
 app.listen(PORT, () => {
